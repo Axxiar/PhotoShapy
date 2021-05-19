8bf1ad8b7688d43c8ae8a1a771ffdeee259b8dbc
MIL0W <81268698+MIL0W@users.noreply.github.com>
2021-05-19 12:21:34 +0200

    docu

--- toplevels.py
+++ toplevels.py
@@ -1,15 +1,22 @@
+###########  IMPORTATION DES MODULES  ###########
 import tkinter as tk                                       
 
 from PIL import Image,ImageTk,ImageFilter,ImageOps
+
 # ----------------------------------------------------------------------------------------------------------------------------------------
 
 
-
 def t_rotate_window(master,lbl,im):
+    """attributions : bouton 'Pivoter' l.afpt
+    description : fonction qui permet de faire pivoter/inverser l'image
+    paramètres : master -> fenêtre principale, lbl -> label qui stock l'image, im -> image à modifier"""
     global image
     image = Image.open(im.filename)
     
     def rotate(side):
+        """attributions : toplevel 'rotate_toplvl' l.afpt
+        descritpion : fontion qui permet de faire pivoter l'image selon le choix de l'utilisateur
+        paramètre : side -> choix de l'utilisateur"""
         global photoim, image
 
         if side == 'left':
@@ -23,6 +30,9 @@ def t_rotate_window(master,lbl,im):
 
 
     def flip(orientation):
+        """attributions : toplevel 'rotate_toplvl' l.afpt
+        descritpion : fonction que permet de faire inverser l'image selon le choix de l'utilisateur
+        paramètre : orientation -> choix de l'utilisateur"""
         global photoim, image
 
         if orientation == 'horizontal':
@@ -32,24 +42,13 @@ def t_rotate_window(master,lbl,im):
         photoim = ImageTk.PhotoImage(image)
         lbl.configure(image=photoim)
         lbl.image = photoim
-    # ----------------------------------------------------------------------------------------------------------------------------------------
 
 
-    ####################################      CREATION ET PARAMETRAGE DE LA FENETRE PRINCIPALE       #########################################
-
-    rotate_toplvl = tk.Toplevel(master)                                           # stockage de la fenêtre principale dans la variable master
-    rotate_toplvl.title("Pivoter")                                 # changement du titre de la fenêtre
-    rotate_toplvl.config(bg='#053f5e')                                # changement de l'arrière plan 
-    rotate_toplvl.geometry("250x400")                                # définition de sa taille par défaut à l'ouverture
-    rotate_toplvl.geometry("+150+510")                                  # définition de sa position par défaut à l'ouverture (Décalage_Top ; Décalage_Left)
-
-
-
-
-    # ----------------------------------------------------------------------------------------------------------------------------------------
-
-
-    ######################################      CREATION DES BOITES QUI STRUCTURE LA FENETRE     #############################################
+    rotate_toplvl = tk.Toplevel(master)                                # stockage de la fenêtre principale dans la variable master
+    rotate_toplvl.title("Pivoter")                                     # définition du titre de la fenêtre
+    rotate_toplvl.config(bg='#053f5e')                                 # définition de l'arrière plan 
+    rotate_toplvl.geometry("250x400")                                  # définition de sa taille par défaut 
+    rotate_toplvl.geometry("+440+510")                                 # définition de sa position par défaut (Décalage_Top ; Décalage_Left)
 
     #   argument 1 : la fenêtre ou boîte dans laquelle on place notre nouvelle boîte
     #   argument 2 : définition de la couleure d'arrière-plan de la boîte
@@ -70,17 +69,12 @@ def t_rotate_window(master,lbl,im):
 
     pivoter_lbl = tk.Label(frame1,bg='#053f5e',text="PIVOTER",font=('Consolas',15),fg='#ffd700')
     pivoter_lbl.pack(side=tk.TOP,ipady=25)
-
     right_rotate = tk.Button(frame2, text = "↩",font=('Consolas 20 bold'),bg="#115173",fg="#ffd700",command=lambda:rotate('left'))
     left_rotate_button = tk.Button(frame2, text = "↪",font=('Consolas 20 bold'),bg="#115173",fg="#ffd700",command=lambda:rotate('right'))
-
     retourner_lbl = tk.Label(frame4,bg='#053f5e',text="INVERSER",font=('Consolas',15),fg='#ffd700')
     retourner_lbl.pack(side=tk.TOP,pady=25)
-
     horizontal_flip_button = tk.Button(frame4, text = "↔",font=('Consolas 20 bold'),bg="#115173",fg="#ffd700", command = lambda: flip('horizontal'))
     vertical_flip_button = tk.Button(frame4, text = "↕",font=('Consolas 20 bold'),bg="#115173",fg="#ffd700",command= lambda: flip('vertical'))
-
-
 
     right_rotate.pack(side=tk.RIGHT,ipadx=10)
     left_rotate_button.pack(side=tk.LEFT,ipadx=10)
@@ -96,10 +90,16 @@ def t_rotate_window(master,lbl,im):
 
 
 def t_filters_window(master, lbl, im):
+    """attributions : bouton 'Filtres' l.afpt
+    description : fonction qui permet de mettre des filtres à l'image
+    paramètres : master -> fenêtre principale, lbl -> label qui stock l'image, im -> image à modifier"""
     global image
     image = Image.open(im.filename)
 
     def filtre(choix):
+        """attributions : toplevel 'filters_toplvl' l.afpt
+        descritpion : fonction que permet de mettre des filtres à l'image selon le choix de l'utilisateur
+        paramètre : choix -> choix de l'utilisateur"""
         global photoim, image
 
         if choix == 'filtre1':
@@ -128,11 +128,15 @@ def t_filters_window(master, lbl, im):
         
 
 
-    filters_toplvl = tk.Toplevel(master)
-    filters_toplvl.title("Filtres")
-    filters_toplvl.config(bg='#053f5e')
-    filters_toplvl.geometry("380x530")
-    filters_toplvl.geometry("+425+200")
+    filters_toplvl = tk.Toplevel(master)                               # stockage de la fenêtre principale dans la variable master
+    filters_toplvl.title("Filtres")                                    # définition du titre de la fenêtre
+    filters_toplvl.config(bg='#053f5e')                                # définition de l'arrière plan 
+    filters_toplvl.geometry("350x600")                                 # définition de sa taille par défaut 
+    filters_toplvl.geometry("+440+200")                                # définition de sa position par défaut (Décalage_Top ; Décalage_Left)
+
+
+    #   argument 1 : la fenêtre ou boîte dans laquelle on place notre nouvelle boîte
+    #   argument 2 : définition de la couleure d'arrière-plan de la boîte
 
     frame = tk.Frame(filters_toplvl,bg='#053f5e')
     frame1 = tk.Frame(filters_toplvl,bg='#053f5e')
@@ -141,7 +145,7 @@ def t_filters_window(master, lbl, im):
     
     frame.pack(ipady=5)
 
-    filtre_lbl = tk.Label(frame,bg='#053f5e',text="CHOISIR UN FILTRE",font=('Consolas',25),fg='#ffd700')
+    filtre_lbl = tk.Label(frame,bg='#053f5e',text="CHOISIR UN FILTRE",font=('Consolas bold',20),fg='#ffd700')
     filtre_lbl.pack(side=tk.TOP,ipady=10,pady=8)
 
     filtre_1 = tk.Button(filters_toplvl, text = "Noir et Blanc",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre3'))
@@ -150,27 +154,26 @@ def t_filters_window(master, lbl, im):
     filtre_4 = tk.Button(filters_toplvl, text = "Affûter",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre2'))
     filtre_5 = tk.Button(filters_toplvl, text = "Contraster",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre6'))
     filtre_6 = tk.Button(filters_toplvl, text = "Gaufrer",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre7'))
-    filtre_7 = tk.Button(frame2, text = "Contours",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre4'))
-    filtre_8 = tk.Button(frame2, text = "Négatif",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre8'))
+    filtre_7 = tk.Button(filters_toplvl, text = "Contours",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre4'))
+    filtre_8 = tk.Button(filters_toplvl, text = "Négatif",font=('Consolas 15',19),bg="#115173",fg="#ffd700",command=lambda:filtre('filtre8'))
 
-    filtre_1.pack(ipady=1,pady=5)
+    filtre_1.pack(ipady=1,pady=5, ipadx=4)
     frame1.pack(ipadx=5)
-    filtre_2.pack(ipady=1,pady=5,ipadx=4,side=tk.LEFT)
-    filtre_3.pack(ipady=1,pady=5,ipadx=4,side=tk.RIGHT)
-    filtre_4.pack(ipady=1,pady=5) 
-    filtre_5.pack(ipady=1,pady=5)
-    filtre_6.pack(ipady=1,pady=5)
-    frame2.pack(pady=5)
-    filtre_7.pack(ipady=1,pady=5,side=tk.LEFT)
-    filtre_8.pack(ipady=1,pady=5,side=tk.RIGHT)
+    filtre_2.pack(ipady=1,pady=5, ipadx=3,side=tk.LEFT)
+    filtre_3.pack(ipady=1,pady=5, ipadx=5,side=tk.RIGHT)
+    filtre_4.pack(ipady=1,pady=5, ipadx=39) 
+    filtre_5.pack(ipady=1,pady=5, ipadx=18)
+    filtre_6.pack(ipady=1,pady=5, ipadx=34)
+    filtre_7.pack(ipady=1,pady=5, ipadx=25)
+    filtre_8.pack(ipady=1,pady=5, ipadx=36)
 
     frame3.pack()
 
     confirm_button = tk.Button(frame3, text = "V",font=('Consolas 15 bold'),bg="#115173",fg="green")
     close_button = tk.Button(frame3, text = "X",font=('Consolas 15 bold'),bg="#115173",fg="red")
-    confirm_button.pack(side=tk.LEFT,padx=5,ipadx=3)
-    close_button.pack(side=tk.RIGHT,padx=5,ipadx=3)
-# ----------------------------------------------------------------------------------------------------------------------------------------
+    confirm_button.pack(side=tk.LEFT,padx=5,ipadx=3, pady=15)
+    close_button.pack(side=tk.RIGHT,padx=5,ipadx=3, pady=15)
+
 
 
 def t_crop_window(master,lbl,im):
@@ -181,10 +184,12 @@ def t_crop_window(master,lbl,im):
     crop_toplvl.geometry("+350+150")
 
 
+    #   argument 1 : la fenêtre ou boîte dans laquelle on place notre nouvelle boîte
+    #   argument 2 : définition de la couleure d'arrière-plan de la boîte
+
     frame5 = tk.Frame(crop_toplvl,bg='#053f5e')
 
     confirm_button = tk.Button(frame5, text = "V",font=('Consolas 15 bold'),bg="#115173",fg="green")
     close_button = tk.Button(frame5, text = "X",font=('Consolas 15 bold'),bg="#115173",fg="red")
     confirm_button.pack(side=tk.LEFT,padx=5,ipadx=3)
     close_button.pack(side=tk.RIGHT,padx=5,ipadx=3)
-# ----------------------------------------------------------------------------------------------------------------------------------------
